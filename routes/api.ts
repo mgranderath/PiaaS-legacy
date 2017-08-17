a9fb2c0ccaae7040a0b5f1fd8a47b54faa9b13f1
Malte-Granderath <malte.granderath@ibm.com>
2017-08-17 11:19:56 +0200

    wrote server documentation - closes #24

--- routes/api.ts
+++ routes/api.ts
@@ -13,10 +13,18 @@ interface app {
   running: boolean;
 }
 
+/**
+ * Gets all App names
+ * @returns {string[]}
+ */
 const getAppNames = (): string[] => {
   return fs.readdirSync(root).filter((f: string) => fs.statSync(root + '/' + f).isDirectory());
 };
 
+/**
+ * Gets App Infos
+ * @returns {Promise<{[p: string]: app}>}
+ */
 const getAppInfo = async (): Promise<{[name: string]: app}> => {
   const appnames = getAppNames();
   const apps: {[name: string]: app} = {};
@@ -29,6 +37,10 @@ const getAppInfo = async (): Promise<{[name: string]: app}> => {
   return apps;
 };
 
+/**
+ * Gets more detailed App info
+ * @returns {Promise<any>}
+ */
 const getDetails = async () => {
   const appnames = getAppNames();
   const details : any = {};
@@ -39,35 +51,56 @@ const getDetails = async () => {
   return details;
 };
 
+/**
+ * Returns all apps
+ */
 router.get('/', async (req: Request, res: Response) => {
   res.json(await getDetails());
 });
 
+/**
+ * Adds an App
+ */
 router.put('/add', async (req: Request, res: Response) => {
   const app = new App(req.query.name);
   res.json(await app.init());
 });
 
+/**
+ * Removes an App
+ */
 router.put('/remove', async (req: Request, res: Response) => {
   const apps = await getAppInfo();
   res.json({ status: await apps[req.query.name].instance.remove() });
 });
 
+/**
+ * Deploys an App
+ */
 router.put('/push', async (req: Request, res: Response) => {
   const apps = await getAppInfo();
   res.json(await apps[req.query.name].instance.pipeline());
 });
 
+/**
+ * Starts an App
+ */
 router.put('/start', async (req: Request, res: Response) => {
   const apps = await getAppInfo();
   res.json({ status: await apps[req.query.name].instance.start() });
 });
 
+/**
+ * Stops an App
+ */
 router.put('/stop', async (req: Request, res: Response) => {
   const apps = await getAppInfo();
   res.json({ status: await apps[req.query.name].instance.stop() });
 });
 
+/**
+ * Returns the logs of an App
+ */
 router.put('/log', async (req: Request, res: Response) => {
   const apps = await getAppInfo();
   const stream = await apps[req.query.name].instance.logs();
@@ -78,6 +111,9 @@ router.put('/log', async (req: Request, res: Response) => {
   },500);
 });
 
+/**
+ * Checks whether App is running
+ */
 router.put('/running', async (req: Request, res: Response) => {
   const apps = await getAppInfo();
   apps[req.query.name].instance.isRunning()
